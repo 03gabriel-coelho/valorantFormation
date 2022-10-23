19e8aa04d11e829ff6e467d8fcaf95ae86ad8ebb
Hugo <hugoemannuel53@gmail.com>
2022-10-23 15:36:44 -0300

    add get para endpoits utilizados na app e add get de agents na page para teste

--- src/Pages/Cards/index.jsx
+++ src/Pages/Cards/index.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { getAgents } from '../../Api';
 
 function Cards() {
+  const [data, setData] = useState([]);
+  console.log('***data', data);
+
+  const getAllAgents = useCallback(async () => {
+    try {
+      const response = await getAgents();
+      setData(response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllAgents();
+  }, [getAllAgents]);
+
   return (
     <p>Cards</p>
   );
