2d226bf59c86afa25a9458177b2c60b492239767
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-10-23 13:57:56 -0300

    Arquivos setados nas rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Arms from './Pages/Arms';
+import Cards from './Pages/Cards';
+import Agents from './Pages/Agents';
+import Maps from './Pages/Maps';
+import Sprays from './Pages/Sprays';
+import Titles from './Pages/Titles';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
-      <Route path="/cards" />
-      <Route path="/arms" />
-      <Route path="/agents" />
-      <Route path="/maps" />
-      <Route path="/sprays" />
-      <Route path="/titles" />
+      <Route path="/cards" element={ <Cards /> } />
+      <Route path="/arms" element={ <Arms /> } />
+      <Route path="/agents" element={ <Agents /> } />
+      <Route path="/maps" element={ <Maps /> } />
+      <Route path="/sprays" element={ <Sprays /> } />
+      <Route path="/titles" element={ <Titles /> } />
     </Routes>
   );
 }
